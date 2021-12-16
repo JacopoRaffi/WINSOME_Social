e7b5c37c83882667079e533ac35015a11c3043b7
JacopoRaffi <75311297+JacopoRaffi@users.noreply.github.com>
2021-12-16 23:43:09 +0100

    Delete src directory

--- src/ServerFunctions.java
+++
@@ -1,6 +0,0 @@
-import java.rmi.Remote;
-
-public interface ServerFunctions extends Remote {
-    /** metodo per registrare un utente a WINSOME */
-    public boolean userRegister(String username, String password, String tags);
-}
