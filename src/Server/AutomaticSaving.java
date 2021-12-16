156be676dbbf101fa64a649c5aeed362a2d6814e
JacopoRaffi <75311297+JacopoRaffi@users.noreply.github.com>
2021-12-16 23:43:22 +0100

    Add files via upload

---
+++ src/Server/AutomaticSaving.java
@@ -0,0 +1,56 @@
+package Server;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+import com.google.gson.*;
+
+//questo thread salva lo stato del social periodicamente
+public class AutomaticSaving implements Runnable{
+    private File postStatuss;
+    private File usersStatus;
+    private WinsomeSocial social;
+    private final long timelapse;
+
+    public AutomaticSaving(WinsomeSocial social, File usersStatus, File postStatuss, long timeLapse){
+        this.social = social;
+        this.postStatuss = postStatuss;
+        this.usersStatus = usersStatus;
+        this.timelapse = timeLapse;
+    }
+
+    public void run(){
+        while(!Thread.currentThread().isInterrupted()){
+            try{
+                Thread.sleep(timelapse);
+                backupPost();
+                backupUsers();
+            }catch(InterruptedException e){
+                break;
+            }catch(IOException e){
+                continue; //anche se fallisce una volta non voglio che si interrompa il thread
+            }
+        }
+    }
+
+    private void backupPost() throws IOException {
+        FileWriter jsonWriter = new FileWriter(postStatuss);
+        ConcurrentHashMap<Integer, Post> post= social.getSocialPost();
+        Gson gsonBuilder = new GsonBuilder().setPrettyPrinting().create();
+        String jsonString = gsonBuilder.toJson(post);
+        //soluzione temporanea
+        jsonWriter.write(jsonString);
+        jsonWriter.close();
+    }
+
+    private void backupUsers() throws IOException {
+        FileWriter jsonWriter = new FileWriter(usersStatus);
+        ConcurrentHashMap<String, User> post= social.getSocialUsers();
+        Gson gsonBuilder = new GsonBuilder().setPrettyPrinting().create();
+        String jsonString = gsonBuilder.toJson(post);
+        //soluzione temporanea
+        jsonWriter.write(jsonString);
+        jsonWriter.close();
+    }
+}
---
+++ src/Server/WinsomeSocial.java
@@ -0,0 +1,66 @@
+package Server;
+
+import java.io.*;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+import Server.Post;
+import Server.ServerFunctions;
+import Server.User;
+
+public class WinsomeSocial implements ServerFunctions {
+    private ConcurrentHashMap<Integer, Post> socialPost;
+    private ConcurrentHashMap<String, User> socialUsers;
+    private volatile AtomicLong postID;
+
+    public WinsomeSocial(File usersStatus){
+        socialPost = new ConcurrentHashMap<>();
+        socialUsers = new ConcurrentHashMap<>();
+        postID = new AtomicLong(0);
+    }
+
+    public boolean userRegister(String username, String password, String tags){
+        try{
+            User newUser = new User(username, password, tags);
+            if(socialUsers.putIfAbsent(username, newUser) != null){ //aggiungo l'utente registrato
+                return true;
+            }else{
+                return false;
+            }
+        }catch(NoSuchAlgorithmException e){
+            return false;
+        }
+
+    }
+
+    public boolean login(String username, String password){
+        try {
+            return (socialUsers.containsKey(username) && socialUsers.get(username).login(username, password));
+        }catch(NoSuchAlgorithmException e){
+            return false;
+        }
+    }
+
+    public String listFollowed(String username){
+        return socialUsers.get(username).getFollowed().toString();
+    }
+
+    public String showFeed(String username){
+        return socialUsers.get(username).getFeed().toString();
+    }
+
+    public String showBlog(String username){
+        return  socialUsers.get(username).getBlog().toString();
+    }
+
+    //questi due metodi get sono utili per il backup su file json
+    public ConcurrentHashMap<Integer, Post> getSocialPost(){
+        return new ConcurrentHashMap<>(socialPost);
+    }
+
+    public ConcurrentHashMap<String, User> getSocialUsers(){
+        return new ConcurrentHashMap<>(socialUsers);
+    }
+
+}
