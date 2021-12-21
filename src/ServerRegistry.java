c17487709b60b769fa12951d676688e6b509943b
JacopoRaffi <75311297+JacopoRaffi@users.noreply.github.com>
2021-12-21 17:13:43 +0100

    Delete src directory

--- src/ServerRegistry.java
+++
@@ -1,9 +0,0 @@
-
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ServerRegistry extends Remote {
-    /** metodo per registrare un utente a WINSOME */
-    boolean userRegister(String username, String password, String tags, String userAddress) throws RemoteException;
-}
