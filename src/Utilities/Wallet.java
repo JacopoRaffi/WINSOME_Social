dc7c56dc156e031093b05837c586f34cbbbf68e7
JacopoRaffi <75311297+JacopoRaffi@users.noreply.github.com>
2021-12-27 10:19:12 +0100

    Add files via upload

--- src/Utilities/Wallet.java
+++ src/Utilities/Wallet.java
@@ -1,4 +1,30 @@
 package Utilities;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Wallet {
+    private long totale;
+    private List<> transazioni;
+
+    public Wallet(){
+        totale = 0;
+        transazioni = new LinkedList<>();
+    }
+
+    public long getTotale() {
+        return totale;
+    }
+
+    public List<> getTransazioni() {
+        return transazioni;
+    }
+
+    public void setTotale(long totale) {
+        this.totale = totale;
+    }
+
+    public boolean addTransazione(){
+        return transazioni.add();
+    }
 }
