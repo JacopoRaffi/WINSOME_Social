b45de304781eb876fe84df5dc80d8866750465d1
JacopoRaffi <75311297+JacopoRaffi@users.noreply.github.com>
2022-01-02 17:27:51 +0100

    Add files via upload

--- src/ClientThinMain.java
+++ src/ClientThinMain.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.net.URL;
 import java.rmi.server.RMIClassLoader;
+import java.rmi.RMISecurityManager;
 
 public class ClientThinMain {
 
@@ -9,17 +10,19 @@ public class ClientThinMain {
             System.err.println("Usage: java LoadClient <remote URL>");
             System.exit(-1);
         }
+        System.setProperty("java.security.policy", "myGrantAllPolicy.policy");
         String filesName = "./Config/ClientConfig.txt";
         if(Args.length >= 2){
             filesName = Args[1];
         }
-        System.setSecurityManager(new MySecurityManager());
+        System.setSecurityManager(new RMISecurityManager());
         try {
             URL url = new File(Args[0]).toURI().toURL();
             Class<?> clientClasss = RMIClassLoader.loadClass(url, "ClientClass");
             Runnable client = (Runnable) clientClasss.getDeclaredConstructor(String.class).newInstance(filesName);
             client.run();
-        } catch (Exception e) { System.out.println("Exception: " +
+        } catch (Exception e) { 
+            System.out.println("Exception: " +
                 e.getMessage());
             e.printStackTrace();
         }
